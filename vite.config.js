093707daf294898c7b23c74faed4b3c3fd9f79bb
BDH <dh10162@naver.com>
2024-08-18 19:27:45 +0900

    fix:vite-config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  build: {
+    outDir: 'build'
+  },
   server: {
     host: '0.0.0.0',
     port: 80,
